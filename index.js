3120938e8f41918a51b21c26af1a95d1a70299ec
Judy Tuan <judytuna@gmail.com>
2021-04-25 15:49:17 -0700

    Prefix in config file. Responds to meow and info.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const Discord = require('discord.js');
+const config = require('./config.json');
+
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -9,9 +11,17 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-  console.log(message.content);
-  if (message.content === 'meow') {
+  if (!message.content.startsWith(`${config.prefix}`)) {
+    console.log('NOPE');
+    return;
+  }
+
+  if (message.content === `${config.prefix} meow`) {
     message.channel.send('Slimecat meows back.');
+  }
+
+  if (message.content === `${config.prefix} info`) {
+    message.channel.send(`I'm a game Arala made for Ludum Dare 48! Try \`${config.prefix} meow\` and see what I do.`);
   }
 });
 
