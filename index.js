f16aceef739ec8dfca76cf7b6125533166874ae0
Judy Tuan <judytuna@gmail.com>
2021-04-25 16:42:19 -0700

    attempt to voice meow

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 const Discord = require('discord.js');
 const config = require('./config.json');
+const fs = require('fs');
 
 const client = new Discord.Client();
 
@@ -10,11 +11,8 @@ client.once('ready', () => {
   console.log('Ready!');
 });
 
-client.on('message', message => {
-  if (!message.content.startsWith(`${config.prefix}`)) {
-    console.log('NOPE');
-    return;
-  }
+client.on('message', async message => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
   if (message.content === `${config.prefix} meow`) {
     message.channel.send('Slimecat meows back.');
@@ -23,6 +21,30 @@ client.on('message', message => {
   if (message.content === `${config.prefix} info`) {
     message.channel.send(`I'm a game Arala made for Ludum Dare 48! Try \`${config.prefix} meow\` and see what I do.`);
   }
+
+  if (message.content === `${config.prefix} voice meow`) {
+    // Join the same voice channel of the author of the message
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      // Create a dispatcher
+      const dispatcher = connection.play('sounds/meow.mp3');
+
+      dispatcher.on('start', () => {
+        console.log('meow.mp3 is now playing!');
+      });
+
+      dispatcher.on('finish', () => {
+        console.log('meow.mp3 has finished playing!');
+      });
+
+      // Always remember to handle errors appropriately!
+      dispatcher.on('error', console.error);
+    }
+    else {
+      message.channel.send('You\'re not in a voice channel I can join, though!');
+    }
+  }
+
 });
 
 client.login(process.env.DISCORD_TOKEN);
