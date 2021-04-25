d81d6e7b4042fb7776f5fb62183e0694d2e0f764
Judy Tuan <judytuna@gmail.com>
2021-04-25 15:27:14 -0700

    it talks back, but also talks back to itself lol

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ client.once('ready', () => {
   console.log('Ready!');
 });
 
+client.on('message', message => {
+  console.log(message.content);
+  message.channel.send('Slimecat meows back.');
+});
+
 client.login(process.env.DISCORD_TOKEN);
