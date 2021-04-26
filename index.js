58fc1d40d7e429de18020a5ba0d4480fa1fe6b6d
Judy Tuan <judytuna@gmail.com>
2021-04-25 17:44:26 -0700

    Begin adding command structure

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
+const fs = require('fs');
 const Discord = require('discord.js');
 const config = require('./config.json');
-const fs = require('fs');
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.name, command);
+  }
+}
 
 client.once('ready', () => {
   console.log('Ready!');
@@ -43,6 +53,31 @@ client.on('message', async message => {
     else {
       message.channel.send('You\'re not in a voice channel I can join, though!');
     }
+  }
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return;
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
+  }
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
   }
 
 });
