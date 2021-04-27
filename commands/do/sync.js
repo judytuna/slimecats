b4b640e4ddb1edc12af525051ba386af37853fcf
Judy Tuan <judytuna@gmail.com>
2021-04-26 21:04:50 -0700

    Add a sync command

---
+++ commands/do/sync.js
@@ -0,0 +1,13 @@
+const slimecatsDo = require('../../slimecats-do');
+
+// const { makeNewTodo } = require(slimecatsDo);
+
+module.exports = {
+  name: 'sync',
+  description: 'Syncs with the Earthstar pub.',
+  args: false,
+  execute(message) {
+    slimecatsDo.syncOnce();
+    message.channel.send('Synced!');
+  },
+};
